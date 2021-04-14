a2b4dbcb1ac2b90b1b190037d84bc9e9eb9479f5
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-14 23:59:52 +0200

    Correction of list of module loading on game creation

--- NCA-more-module-slots/control.lua
+++ NCA-more-module-slots/control.lua
@@ -1,18 +1,13 @@
 local more_module_slots_defines = require('defines')
+local more_module_slots_data = require('data-updates')
 
 local function set_stack(e)
     local player = game.players[e.player_index]
     if not player.get_main_inventory() then return end
-    
     local itemproto = game.item_prototypes
-    local count = 1
-    
-    for k, v in pairs(data.raw.module) do
-        if str.find(v.name, "module-0") and itemproto[v.name] then
-            if v.in_stack then
-                count = itemproto[v.name].stack_size * 5
-            end
-            player.insert{ name = v.name, count = count }
+    for m in more_module_slots_data.module_list do
+        if itemproto[m] then
+            player.insert{ name = m, count = itemproto[m].stack_size * 5 }
         end
     end
 end
--- NCA-more-module-slots/data-updates.lua
+++ NCA-more-module-slots/data-updates.lua
@@ -1,5 +1,12 @@
 local more_module_slots_defines = require('defines')
+local module_list = {}
+
 
 if settings.startup[more_module_slots_defines.names.settings.extra_modules].value then
     require('prototypes.modules')
+    for k, v in pairs(data.raw.module) do
+        if str.find(v.name, "module-0") then
+            table.insert(module_list, v.name)
+        end
+    end
 end
