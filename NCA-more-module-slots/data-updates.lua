26db5891b9ff89b581df7d7608fa9c0116cd65b0
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-15 00:01:06 +0200

    Correction function string find

--- NCA-more-module-slots/data-updates.lua
+++ NCA-more-module-slots/data-updates.lua
@@ -5,7 +5,7 @@ local module_list = {}
 if settings.startup[more_module_slots_defines.names.settings.extra_modules].value then
     require('prototypes.modules')
     for k, v in pairs(data.raw.module) do
-        if str.find(v.name, "module-0") then
+        if string.find(v.name, "module-0") then
             table.insert(module_list, v.name)
         end
     end
