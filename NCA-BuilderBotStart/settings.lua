2ff56b364010cebf69aff8269d3e82e78fb55a41
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-16 21:29:28 +0200

    BuilderBotStar - Add mod

---
+++ NCA-BuilderBotStart/settings.lua
@@ -0,0 +1,20 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "starting robot count",
+        setting_type = "runtime-global",
+        default_value = 50,
+        minimum_value = 50,
+        maximum_value = 500,
+        order = "01"
+    },
+    {
+        type = "int-setting",
+        name = "faster robots",
+        setting_type = "runtime-global",
+        default_value = 0,
+        minimum_value = 0,
+        maximum_value = 7,
+        order = "02"
+    }
+})
