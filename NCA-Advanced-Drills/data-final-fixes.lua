f8c6dc262b99b7205bb2a2e291691f48fc989d9c
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-17 15:38:40 +0200

    Advanced-Drills - Add configuration for speed

--- NCA-Advanced-Drills/data-final-fixes.lua
+++ NCA-Advanced-Drills/data-final-fixes.lua
@@ -1,9 +1,11 @@
 
 
-local multiplier = 10.0
+local multiplier = settings.startup["NCA-Advanced-Drills-Speed-multiplier"]
 
-for k, v in pairs(data.raw["mining-drill"]) do
-    if v.mining_speed then
-        v.mining_speed = v.mining_speed * multiplier
+if multiplier ~= 1 then
+    for k, v in pairs(data.raw["mining-drill"]) do
+        if v.mining_speed then
+            v.mining_speed = v.mining_speed * multiplier
+        end
     end
 end
---
+++ NCA-Advanced-Drills/settings.lua
@@ -0,0 +1,12 @@
+
+data:extend {
+    {
+        type = "double-setting",
+        name = "NCA-Advanced-Drills-Speed-multiplier",
+        setting_type = "runtime-global",
+        default_value = 1,
+        minimum_value = 0.1,
+        maximum_value = 10.0
+        order = 'aa'
+    }
+}
