33ab4ff220889a68cd5beb18f0d112033276c62f
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-17 17:19:19 +0200

    Starter - Merge stating inventory and bot start

---
+++ NCA-Starter/control.lua
@@ -0,0 +1,186 @@
+
+--- Generation of inventory
+local function Generate_Inventory(e)
+    local player = game.players[e.player_index]
+    if not player.get_main_inventory() then return end
+    local itemproto = game.item_prototypes
+    local inventory = {}
+
+    -- basic resource
+    inventory["coal"]={stack=1}
+    inventory["iron-plate"]={stack=1}
+    inventory["copper-plate"]={stack=1}
+    inventory["steel-plate"]={stack=1}
+    inventory["stone-brick"]={stack=1}
+    
+    -- Power
+    inventory["medium-electric-pole"]={stack=2}
+    inventory["solar-panel"]={stack=4}
+    inventory["accumulator"]={stack=3}
+    
+    -- Other
+    inventory["steel-chest"]={stack=1}
+    inventory["small-lamp"]={stack=2}
+            
+    -- Fluid
+    inventory["pipe"]={stack=1}
+    inventory["pipe-to-ground"]={stack=1}
+    if game.active_mods["inland_pumps"] then
+        inventory["offshore-pump-inland"]={stack=1}
+    else
+        inventory["offshore-pump"]={stack=1}
+    end
+    inventory["pump"]={stack=1}
+    inventory["storage-tank"]={stack=1}
+    
+    -- Machine
+    inventory["electric-mining-drill"]={stack=2}
+    inventory["electric-furnace"]={stack=4}
+    inventory["assembling-machine-1"]={stack=2}
+    inventory["lab"]={stack=1}
+    
+    -- Logistic
+    inventory["transport-belt"]={stack=4}
+    inventory["underground-belt"]={stack=1}
+    inventory["splitter"]={stack=1}
+    inventory["loader"]={stack=1}
+    inventory["inserter"]={stack=4}
+
+    -- Train
+    inventory["locomotive"]={stack=1}
+    inventory["cargo-wagon"]={stack=2}
+    inventory["fluid-wagon"]={stack=2}
+    inventory["rail"]={stack=4}
+    inventory["train-stop"]={stack=2}
+    inventory["rail-signal"]={stack=2}
+    inventory["rail-chain-signal"]={stack=2}    
+    
+    
+    -- ammo and explosive
+    inventory["firearm-magazine"]={stack=2}
+    inventory["cliff-explosives"]={stack=2}
+
+		
+    -- Find if module list exist in item prototype
+    local is_module_0 = false
+    for name,proto in pairs(itemproto) do
+        if proto.type == "module" then
+            if string.find(name, "module%-0") then
+                is_module_0 = true
+                break
+            end
+        end
+    end
+    -- Create list of module
+    local module_stack = 2
+    if is_module_0 then
+        inventory["speed-module-0"] = {stack=module_stack}
+        inventory["effectivity-module-0"] = {stack=module_stack}
+        inventory["productivity-module-0"] = {stack=module_stack}
+    else
+        inventory["speed-module"] = {stack=module_stack}
+        inventory["effectivity-module"] = {stack=module_stack}
+        inventory["productivity-module"] = {stack=module_stack}
+    end
+    -- Insert all element
+    for name, proto in pairs(inventory) do
+        if itemproto[name] then
+            player.print("Add " .. itemproto[name].stack_size * proto.stack .. " of ".. name .. ".")
+            player.insert{ name = name, count = itemproto[name].stack_size * proto.stack }
+        end
+    end
+end
+
+function Generate_Armor(e)
+	local ItemPrototypes = {
+		Armor = "power-armor-mk2",
+		Robot = "construction-robot",
+		Fuel = "",
+		Reactor = "fusion-reactor-equipment", --4x4
+		Exoskeleton = "exoskeleton-equipment", --2x4
+		Shield = "energy-shield-mk2-equipment", --2x2
+		Roboport = "personal-roboport-mk2-equipment", --2x2
+		Battery = "battery-mk2-equipment", --1x2
+		LaserDefense = "personal-laser-defense-equipment", --2x2	
+		Nightvision = "night-vision-equipment", --2x2
+		Panel = "solar-panel-equipment", --1x1
+		Immunity = "belt-immunity-equipment", --1x1
+	}
+
+	local Items = {{ItemPrototypes["Robot"], settings.global["NCA-Starter-robots-count"].value}}
+	local ArmorModules = {
+		--Vanilla, 10x10 grid
+		{Name = ItemPrototypes["Roboport"], Count = 4},
+		{Name = ItemPrototypes["Reactor"], Count = 4},
+		{Name = ItemPrototypes["Battery"], Count = 3},
+		{Name = ItemPrototypes["Panel"], Count = 1},
+		{Name = ItemPrototypes["Immunity"], Count = 1},
+		{Name = ItemPrototypes["Nightvision"], Count = 1},
+		{Name = ItemPrototypes["Exoskeleton"], Count = 1},
+	}
+	local Player = game.players[e.player_index]
+	local ArmorInventory = Player.get_inventory(defines.inventory.character_armor)
+	
+	if not(ArmorInventory == nil) then --If the player doesn't have armor inventory, the player hasn't spawned, so we can skip this round.
+		for i, v in pairs(Items) do
+			Player.insert{name = v[1], count = v[2]}
+		end
+		if not(ArmorInventory.is_empty()) then
+			--We want to remove whatever armor they had to slot in what we want.
+			local CurrentArmor = ArmorInventory[1].name
+			ArmorInventory.clear()
+			--Then for good measure we destroy it from the inventory.
+			local PlayerInventory = Player.get_inventory(defines.inventory.character_main)
+			PlayerInventory.remove(CurrentArmor);
+		end
+		local n = 0
+		n = ArmorInventory.insert{name=ItemPrototypes["Armor"],count=1}
+		if(n > 0)then -- we actually equipped the armor
+			local grid=ArmorInventory[1].grid
+			for i,module in pairs(ArmorModules) do
+				for y = 1, module.Count, 1 do
+					grid.put({name=module.Name})
+				end
+			end
+		end
+	end
+end	
+
+
+--Freeplay
+script.on_init(function(e)
+	if settings.global["NCA-Starter-starting-robots"].value == false then return end
+	local speed = settings.global["NCA-Starter-robots-speed"].value
+	if not(speed == 0) then
+		for k,v in pairs(game.forces) do
+			for z = 1, speed, 1 do
+				if z < 6 then
+					v.technologies["worker-robots-speed-" .. tostring(z)].researched = true
+				else
+					v.technologies["worker-robots-speed-6"].level = v.technologies["worker-robots-speed-6"].level + 1
+				end
+			end
+		end
+	end
+end)
+
+--Classic start/no cutscene/multiplayer addition
+script.on_event(defines.events.on_player_created, function(e)
+	if settings.global["NCA-Starter-starting-robots"].value == true then
+		Generate_Armor(e)
+	end
+	if settings.global["NCA-Starter-starting-items"].value == true then
+		Generate_Inventory(e)
+	end
+end)
+
+--Freeplay/Cutscene start
+script.on_event(defines.events.on_cutscene_cancelled, function(e)
+	if settings.global["NCA-Starter-starting-robots"].value == true then
+		Generate_Armor(e)
+	end
+	if remote.interfaces["freeplay"] and settings.global["NCA-Starter-starting-items"].value == true then
+		Generate_Inventory(e)
+	end
+end)
+
---
+++ NCA-Starter/settings.lua
@@ -0,0 +1,38 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "NCA-Starter-starting-items",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "aa"
+
+    }
+    ,
+    {
+        type = "bool-setting",
+        name = "NCA-Starter-starting-robots",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "ba"
+
+    }
+    ,
+    {
+        type = "int-setting",
+        name = "NCA-Starter-robots-count",
+        setting_type = "runtime-global",
+        default_value = 50,
+        minimum_value = 50,
+        maximum_value = 500,
+        order = "bb"
+    }
+    ,
+    {
+        type = "int-setting",
+        name = "NCA-Starter-robots-speed",
+        setting_type = "runtime-global",
+        default_value = 0,
+        minimum_value = 0,
+        order = "bc"
+    }
+})
