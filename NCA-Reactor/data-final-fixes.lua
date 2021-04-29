197a1f4567b00436ebba6f6ec70c5252ba45427c
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-29 22:57:19 +0200

    Reactor - Correction power as string

--- NCA-Reactor/data-final-fixes.lua
+++ NCA-Reactor/data-final-fixes.lua
@@ -2,7 +2,9 @@
 local power_multiplier = settings.startup["NCA-Reactor-multiplier"].value
 
 if power_multiplier ~= 1 or logistic_multiplier ~= 1then
-	data.raw["generator-equipment"]["fusion-reactor-equipment"].power = data.raw["generator-equipment"]["fusion-reactor-equipment"].power * power_multiplier
+	local power_str = string.gsub(data.raw["generator-equipment"]["fusion-reactor-equipment"].power,"kW")
+	local power = tonumber(power_str) * power_multiplier
+	data.raw["generator-equipment"]["fusion-reactor-equipment"].power = tostring(math.floor(power)) ... "kW"
 end
 
 
