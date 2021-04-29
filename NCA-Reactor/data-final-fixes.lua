576259240a73d8b1602374311a9a3898e9168c7b
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-04-29 22:59:14 +0200

    Reactor - String concatenation error

--- NCA-Reactor/data-final-fixes.lua
+++ NCA-Reactor/data-final-fixes.lua
@@ -4,7 +4,7 @@ local power_multiplier = settings.startup["NCA-Reactor-multiplier"].value
 if power_multiplier ~= 1 or logistic_multiplier ~= 1then
 	local power_str = string.gsub(data.raw["generator-equipment"]["fusion-reactor-equipment"].power,"kW")
 	local power = tonumber(power_str) * power_multiplier
-	data.raw["generator-equipment"]["fusion-reactor-equipment"].power = tostring(math.floor(power)) ... "kW"
+	data.raw["generator-equipment"]["fusion-reactor-equipment"].power = tostring(math.floor(power)) .. "kW"
 end
 
 
