032e8a280c750e67260e16409e4304a0ec30e274
Nicolas CAMUS <nicolascamus08@gmail.com>
2021-11-11 15:55:11 +0100

    List of stack objects and simplification of conf

--- NCA-Stack/settings.lua
+++ NCA-Stack/settings.lua
@@ -9,24 +9,4 @@ data:extend {
         maximum_value = 100,
         order = 'aa'
     }
-    ,
-    {
-        type = "double-setting",
-        name = "NCA-Stack-logistic-multiplier",
-        setting_type = "startup",
-        default_value = 10,
-        minimum_value = 1,
-        maximum_value = 100,
-        order = 'ab'
-    }
-    -- ,
-    -- {
-    --     type = "int-setting",
-    --     name = "NCA-Stack-ammo-multiplier",
-    --     setting_type = "startup",
-    --     default_value = 10,
-    --     minimum_value = 1,
-    --     maximum_value = 100,
-    --     order = 'ac'
-    -- }
 }
